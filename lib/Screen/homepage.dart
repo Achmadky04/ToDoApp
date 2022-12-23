a72bd6f99e0c93ef2a3adac20bbbe29cc7381aa8
Achmad Rizky <Achmad.Mauludi@bni.co.id>
2022-12-23 15:37:53 +0700

    add List View and Search Bar

--- lib/Screen/homepage.dart
+++ lib/Screen/homepage.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,12 +8,61 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool varSementara = true;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-       body: Center(
-          child: Text("Hello World"),
-        ),
+    AppBar myAppBar = AppBar(
+      title: Text("Jadwalin Yuk"),
+      elevation: 0,
+      backgroundColor: Colors.transparent,
+      centerTitle: true,
     );
+
+    double heighBody = MediaQuery.of(context).size.height -
+        myAppBar.preferredSize.height -
+        MediaQuery.of(context).padding.top;
+    double widthBody = MediaQuery.of(context).size.width;
+    return Scaffold(
+        appBar: myAppBar,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                width: widthBody,
+                height: heighBody * 0.15,
+                // color: Colors.amber,
+                padding: const EdgeInsets.all(20),
+                child: const TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.search),
+                      hintText: "Cari Jadwalmu di Sini"),
+                ),
+              ),
+              Container(
+                  // padding: const EdgeInsets.symmetric(horizontal: 20),
+                  height: heighBody * 0.85,
+                  width: widthBody,
+                  // color: Colors.amber,
+                  child: ListView.builder(
+                    itemCount: 10,
+                    itemBuilder: ((context, index) {
+                      return CheckboxListTile(
+                        secondary: IconButton(onPressed: () {}, icon: Icon(Icons.delete,
+                          color: Colors.red,)),
+                        controlAffinity: ListTileControlAffinity.leading,
+                        title: const Text(
+                          "Title",
+                          style: TextStyle(fontSize: 17),
+                        ),
+                        subtitle: const Text("Subtitle"),
+                        value: varSementara,
+                        onChanged: (value) {},
+                      );
+                    }),
+                  ))
+            ],
+          ),
+        ));
   }
 }
